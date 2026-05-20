3fadd6451c6a4980080cdb464a7b5f4ed2925d02
IAmOkey <artem.to2018@yandex.ru>
2026-05-20 19:40:53 +0300

    добавил докерфайл

--- lib/service-config.ts
+++ lib/service-config.ts
@@ -1,8 +1,8 @@
 export const SERVICE_URLS = {
-  swagger: 'http://localhost:8082',
-  photo: 'http://localhost:8001',
-  video: 'http://localhost:5000',
-  repoReview: 'http://localhost:8083',
-} as const
+  swagger: 'http://x10group.digital:8082',
+  photo:   'http://x10group.digital:8001',
+  video:   'http://x10group.digital:5000',
+  repoReview: 'http://x10group.digital:8083',
+}
 
 export type ServiceKey = keyof typeof SERVICE_URLS
